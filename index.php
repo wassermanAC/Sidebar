2f75e284a08e6f5d95218d1c3dd03ab5ab947d55
Fitri Lestari <fitrilestari.github@gmail.com>
2021-01-24 21:47:54 +0700

    Sidebar

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIDEBAR</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+    <input type="checkbox" id="check">
+        <label for="check">
+            <i class="fas fa-bars" id="btn"></i>
+            <i class="fas fa-times" id="cancel"></i>
+        </label>
+    <div class="sidebar">
+        <header>Cryptography App</header>
+            <ul>
+                <li><a href=""><i class="fas fa-qrcode"></i>Dashboard</a></li>
+                <li><a href=""><i class="fas fa-link"></i>Introduction</a></li>
+                <li><a href=""><i class="fas fa-text-height"></i>Encryption</a></li>
+                <li><a href=""><i class="fas fa-text-width"></i>Decryption</a></li>
+                <li><a href=""><i class="fas fa-question-circle"></i>About</a></li>
+                <li><a href=""><i class="fas fa-sliders-h"></i>Services</a></li>
+                <li><a href=""><i class="fas fa-envelope"></i>Contact</a></li>
+            </ul>
+    </div>
+    <section></section>
+</body>
+</html>
